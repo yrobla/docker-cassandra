74a9c664cf2bb3b66711a3ac8f357c476fb36d15
Yolanda Robla <yolanda.robla-mota@hp.com>
2014-10-27 15:12:51 +0100

    Added initial configuration for mesos

--- cassandra/scripts/cassandra-singlenode.sh
+++ cassandra/scripts/cassandra-singlenode.sh
@@ -2,8 +2,9 @@
 
 # Get running container's IP
 IP=`hostname --ip-address`
-if [ $# == 1 ]; then SEEDS="$1,$IP"; 
-else SEEDS="$IP"; fi
+
+if [ $# == 1 ]; then SEEDS="$1,$HOST"; 
+else SEEDS="$HOST"; fi
 
 
 # Dunno why zeroes here
@@ -15,6 +16,9 @@ sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \"$SEEDS\"/" $CASSANDRA_CONFIG/cass
 # Listen on IP:port of the container
 sed -i -e "s/^listen_address.*/listen_address: $IP/" $CASSANDRA_CONFIG/cassandra.yaml
 
+# Broadcast on IP:port of the container
+sed -i -e "s/^broadcast_address.*/listen_address: $HOST/" $CASSANDRA_CONFIG/cassandra.yaml
+
 # With virtual nodes disabled, we need to manually specify the token
 echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.initial_token=0\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
@@ -22,12 +26,6 @@ echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.initial_token=0\"" >> $CASSANDRA_CONFIG/
 echo "JVM_OPTS=\"\$JVM_OPTS -Dcassandra.skip_wait_for_gossip_to_settle=0\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
 # Most likely not needed
-echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$IP\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
-
-# If configured in $CASSANDRA_DC, set the cassandra datacenter.
-if [ ! -z "$CASSANDRA_DC" ]; then
-    sed -i -e "s/endpoint_snitch: SimpleSnitch/endpoint_snitch: PropertyFileSnitch/" $CASSANDRA_CONFIG/cassandra.yaml
-    echo "default=$CASSANDRA_DC:rac1" > $CASSANDRA_CONFIG/cassandra-topology.properties
-fi
+echo "JVM_OPTS=\"\$JVM_OPTS -Djava.rmi.server.hostname=$HOST\"" >> $CASSANDRA_CONFIG/cassandra-env.sh
 
 cassandra -f
